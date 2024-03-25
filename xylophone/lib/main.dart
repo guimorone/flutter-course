50ab894cb6c4a4084b5333502cdaf7f0f76977f5
Guilherme <guimorone@gmail.com>
2024-03-25 16:58:02 -0300

    xylophone added

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(const XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  static void play(String src) async {
+    final AudioPlayer player = AudioPlayer();
+    await player.setSourceAsset(src);
+    await player.resume();
+  }
+
+  static Widget buildKey(int sound, Color color) => Expanded(
+        child: TextButton(
+          onPressed: () => play('sounds/note$sound.wav'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(color),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+                side: BorderSide(color: color),
+              ),
+            ),
+          ),
+          child: Container(
+            child: null,
+          ),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
